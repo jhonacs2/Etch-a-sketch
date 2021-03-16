e90b30782c69507dcc00326c63f47458c9c182f3
Jhonatan Soto <jhonrocker2012@gmail.com>
2021-03-15 21:54:15 -0400

    Etch-a-Sketch 1.0

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
 const main = document.querySelector('#main');
-let size = 2;
+const changueSize = document.querySelector('#changueS');
+
+window.addEventListener("load", createGrid());
 
 
+//changue the size of the grid
+changueSize.addEventListener('click', () => {
+    const mainArray = Array.from(main.childNodes);
+    mainArray.forEach((element) => {main.removeChild(element)});
 
+    createGrid(Number(prompt('Insert the Number',2)));
+    
+});
+
+//this function create a random number for color grid
+function changueColor(){
+    let randomNumber = Math.floor(Math.random() * (256 - 1 + 1)) + 1;
+    return randomNumber;
+}
 //the logic of the grid was created in a few words
-//size of th grid 
-for (let i = 0 ; i<size*size; i++){
+//size of the grid
+function createGrid(size = 4) {
+    
+    if ( size > 64 || isNaN(size)) return alert('the number has to be less than 64');
+    
+    for (let i = 0 ; i<size*size; i++){
     const createDivs = document.createElement('div');
     createDivs.style.width = `${(100/size)-0.4}%`;
     createDivs.classList.add('grid');
     main.appendChild(createDivs);
-    console.log(i);
 }
+    mouseOver();
+}
+
+//fuction changue the color of the div when the mouse is over
+function mouseOver (){
+    const selectDivs  = document.querySelectorAll('.main > div');
+    
+    selectDivs.forEach( divs => divs.addEventListener('mouseover',function(e){
+        divs.style.background = `rgb(${changueColor()}, ${changueColor()}, ${changueColor()})`;
+}))};
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
